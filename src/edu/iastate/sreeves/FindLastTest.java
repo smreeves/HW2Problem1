6a13c2de6a97e664406f0767870fa3852c53fb1c
Sam Reeves <sreeves@iastate.edu>
2016-02-19 13:52:39 -0600

    Moved findLast class and test to project.

---
+++ src/edu/iastate/sreeves/FindLastTest.java
@@ -0,0 +1,21 @@
+package edu.iastate.sreeves;
+
+//Introduction to Software Testing
+//Authors: Paul Ammann & Jeff Offutt
+//Chapter 1, page ??
+//JUnit tests for FindLast.java 
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class FindLastTest
+{
+// this test fails!
+@Test public void lastOccurrenceInFirstElement() 
+{
+   int arr[] = {2, 3, 5};
+   int y = 2;
+   assertEquals("Last occurence in first element", 0, FindLast.findLast(arr, y));
+}
+}
